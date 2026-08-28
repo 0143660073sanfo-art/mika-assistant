edb55ebaae0bc29463bb8388596471c7913e714f
Mika <0143660073sanfo@gmail.com>
2026-08-28 01:44:19 +0000

    Suppression du fichier de test

--- test-memory.js
+++
@@ -1,7 +0,0 @@
-const { extractMemory } = require("./src/ai/memoryExtractor");
-
-extractMemory("Mon projet est Mika Assistant");
-extractMemory("Mon objectif est de créer une IA personnelle");
-extractMemory("J'étudie en terminale scientifique");
-extractMemory("Mes compétences sont JavaScript, informatique et bureautique");
-extractMemory("J'aime le football");
